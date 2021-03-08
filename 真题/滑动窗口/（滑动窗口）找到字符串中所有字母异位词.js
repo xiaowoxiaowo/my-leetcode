304ef4b4c7050ae6b123f627305cd90f11ffc133
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-03-09 01:19:42 +0800

    'feat:找到字符串中所有字母异位词'

--- 真题/滑动窗口/（滑动窗口）找到字符串中所有字母异位词.js
+++ 真题/滑动窗口/（滑动窗口）找到字符串中所有字母异位词.js
@@ -17,20 +17,34 @@
  * 
  * 
  */
-function isAnagrams(str1, str2) {
-	return true;
-}
 
 var findAnagrams = function(s, p) {
-	const sLen = s.length, plen = p.lengh;
+	const sLen = s.length, pLen = p.lengh, pMap = new Map();
 	if (sLen < 1) return -1;
-	let l = 0, r = plen - 1, res = [];
+	for (let i = 0; i < p.length; i++) {
+		pMap.set(p[i], pMap.has(p[i]) ? pMap.get(p[i]) + 1 : 1);
+	}
+	let l = 0, r = 0, res = [], sMap = new Map(), valid = 0;
 	while (r < sLen) {
-		if (isAnagrams(s[l], s[r])) {
-			res.push(l);
+		if (pMap.has(s[r])) {
+			sMap.set(s[r], sMap.has(s[r]) ? sMap.get(s[r]) + 1 : 1);
+			if (sMap.get(s[r]) === pMap.get(s[r])) {
+				valid++;
+			}
 		}
 		r++;
-		l++;
+		if (r - l >= pLen) {
+			if (valid === pMap.size) {
+				res.push(l);
+			}
+			sMap.set(s[l], sMap.get(s[l]) -1);
+			if (sMap.get(s[l]) + 1 === pMap.get(s[l])) {
+				valid--;
+			} else if (sMap.get(s[l]) === pMap.get(s[l])) {
+				valid++;
+			}
+			i++;
+		}
 	}
 	return res;
 };
@@ -39,3 +53,85 @@ console.log(findAnagrams("cbaebabacd", "abc"));
 console.log('正确输出：[0, 6]');
 console.log(findAnagrams("abab", "ab"));
 console.log('正确输出：[0, 1, 2]');
+
+
+
+
+
+
+
+
+
+
+
+
+// var findAnagrams = function(s, p) {
+// 	const res = []
+// 	const need = new Map()
+// 	for(let i = 0; i < p.length; i++) {
+// 			need.set(p[i], need.has(p[i])?need.get(p[i])+1: 1)
+// 	}
+// 	let left = 0, right = 0, valid = 0
+// 	// 用于统计窗口中的字符
+// 	const window = new Map()
+// 	// 遍历s串
+// 	while(right < s.length) {
+// 			// 进入窗口的字符
+// 			const c = s[right]
+// 			// 扩大窗口
+// 			right++
+// 			// 进入窗口的字符是所需字符
+// 			if (need.has(c)) {
+// 					// 更新滑动窗口中的字符记录
+// 					window.set(c, window.has(c)?window.get(c)+1:1)
+// 					// 当窗口中的字符数和滑动窗口中的字符数一致
+// 					if (window.get(c) === need.get(c)) {
+// 							// 有效字符自增
+// 							valid++
+// 					}
+// 			}
+// 			// 当滑动窗口的大小超出p串长度时 收缩窗口
+// 			while (right - left >= p.length) {
+// 					// 有效字符和所需字符数一致 找到一条符合条件的子串
+// 					if (valid === need.size) {
+// 							// 保存子串的起始索引位置
+// 							res.push(left)
+// 					}
+// 					// 离开窗口的字符
+// 					const d = s[left]
+// 					// 收缩窗口
+// 					left++
+// 					// 如果离开窗口字符是所需字符
+// 					if (need.has(d)) {
+// 							// 如果离开字符数和所需字符数一致
+// 							if (window.get(d) === need.get(d)) {
+// 									// 有效字符减少一个
+// 									valid--
+// 							}
+// 							// 更新滑动窗口中的字符数
+// 							window.set(d, window.get(d)-1)
+// 					}
+// 			}
+// 	}
+// 	// 返回结果
+// 	return res
+// };
+
+var findAnagrams = function (s, p) {
+	const res = [], win = {}, need = {}, pLen = p.length;
+	let len = 0, val = 0;
+	for (const x of p) {
+			if (need[x] === undefined) {
+					need[x] = win[x] = 0;
+					len++;
+			}
+			need[x]++;
+	}
+	for (let i = 0; i < s.length; i++) {
+			const j = i - pLen;
+			if (s[i] in need && ++win[s[i]] === need[s[i]]) val++;
+			if (s[j] in need && win[s[j]]-- === need[s[j]]) val--;
+			if (val === len) res.push(j + 1);
+	}
+	return res;
+};
