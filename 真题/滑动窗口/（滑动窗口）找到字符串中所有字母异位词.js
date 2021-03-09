8c03c2ece1ce11d7742b79216f67c1fdc87a536f
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-03-09 19:42:57 +0800

    *

--- 真题/滑动窗口/（滑动窗口）找到字符串中所有字母异位词.js
+++ 真题/滑动窗口/（滑动窗口）找到字符串中所有字母异位词.js
@@ -15,54 +15,50 @@
  * 输入：s: "abab" p: "ab"
  * 输出：[0, 1, 2]
  * 
- * 
+ * 解答思路：
+ * 本题重点其实就在怎么判断s中的子串是否是p字符串的异位词。通过map类型（主要是可以使用pMap.size得到该
+ * map里key的数量，也可以用对象来实现，不过需要自己维护一个key的数量）key的数量来判断异位词，只要key的数量一致，
+ * 每个key存在个数是一致的，就可以判断符合异位词。再通过滑动窗口，得到最终的结论。
  */
 
 var findAnagrams = function(s, p) {
-	const sLen = s.length, pLen = p.lengh, pMap = new Map();
-	if (sLen < 1) return -1;
+	const sLen = s.length, pLen = p.length, pMap = new Map();
+	if (sLen < 1) return [];
+	// 初始化梳理p字符串
 	for (let i = 0; i < p.length; i++) {
 		pMap.set(p[i], pMap.has(p[i]) ? pMap.get(p[i]) + 1 : 1);
 	}
 	let l = 0, r = 0, res = [], sMap = new Map(), valid = 0;
 	while (r < sLen) {
-		if (pMap.has(s[r])) {
-			sMap.set(s[r], sMap.has(s[r]) ? sMap.get(s[r]) + 1 : 1);
-			if (sMap.get(s[r]) === pMap.get(s[r])) {
+		const cR = s[r];
+		r++;
+		// 只需要去判断符合p字符串特性的字符
+		if (pMap.has(cR)) {
+			// 增加字符串个数
+			sMap.set(cR, sMap.has(cR) ? sMap.get(cR) + 1 : 1);
+			// 如果某个字符串数量跟p里的字符个数相等
+			if (sMap.get(cR) === pMap.get(cR)) {
 				valid++;
 			}
 		}
-		r++;
+		// 上面已经给r加1了，所以这里只需要判断r - l === pLen。在子串长度跟p相等时，去判断是否符合异位词。
 		if (r - l >= pLen) {
 			if (valid === pMap.size) {
 				res.push(l);
 			}
-			sMap.set(s[l], sMap.get(s[l]) -1);
-			if (sMap.get(s[l]) + 1 === pMap.get(s[l])) {
-				valid--;
-			} else if (sMap.get(s[l]) === pMap.get(s[l])) {
-				valid++;
+			const cL = s[l];
+			// 滑动窗口要右移了，i加1之前，判断s[l]当前的字符是否符合验证条件，如果符合，给valid减1。
+			if (pMap.has(cL)) {
+				if (sMap.get(cL) === pMap.get(cL)) {
+					valid--;
+				}
+				sMap.set(cL, sMap.get(cL) - 1);
 			}
-			i++;
+			l++;
 		}
 	}
 	return res;
 };
-
-console.log(findAnagrams("cbaebabacd", "abc"));
-console.log('正确输出：[0, 6]');
-console.log(findAnagrams("abab", "ab"));
-console.log('正确输出：[0, 1, 2]');
-
-
-
-
-
-
-
-
-
-
 
 
 // var findAnagrams = function(s, p) {
@@ -116,22 +112,3 @@ console.log('正确输出：[0, 1, 2]');
 // 	// 返回结果
 // 	return res
 // };
-
-var findAnagrams = function (s, p) {
-	const res = [], win = {}, need = {}, pLen = p.length;
-	let len = 0, val = 0;
-	for (const x of p) {
-			if (need[x] === undefined) {
-					need[x] = win[x] = 0;
-					len++;
-			}
-			need[x]++;
-	}
-	for (let i = 0; i < s.length; i++) {
-			const j = i - pLen;
-			if (s[i] in need && ++win[s[i]] === need[s[i]]) val++;
-			if (s[j] in need && win[s[j]]-- === need[s[j]]) val--;
-			if (val === len) res.push(j + 1);
-	}
-	return res;
-};
