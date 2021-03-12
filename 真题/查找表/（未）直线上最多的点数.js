16ad937ec0de84de4f0d78a0bbfbd519a75d0097
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-03-12 18:34:43 +0800

    *

--- 真题/查找表/（未）直线上最多的点数.js
+++ 真题/查找表/（未）直线上最多的点数.js
@@ -13,7 +13,29 @@
  * 
  * 
  */
+ [[0,0],[1,-1],[1,1]]
+
+var getC = (point1, point2) => {
+  if (point1[0] === point2[0]) return `x${point1[0]}`;
+  if (point1[1] === point2[1]) return `y${point1[1]}`;
+  const s = Math.round(((point1[0] - point2[0])/(point1[1] - point2[1]))*100)/100;
+  return `${s},${point1[1] - (point1[0] * s)}`;
+};
 
 var maxPoints = function(points) {
-
+  const len = points.length;
+  if (len <= 2) return len;
+  let map = new Map(), res = 0;
+  for (let i = 0; i < len; i ++) {
+    for (let j = i + 1; j < len; j ++) {
+      const c = getC(points[i], points[j]);
+      map.set(c, map.has(c) ? map.get(c) + 1 : 1);
+    }
+    let max = Math.max(...map.values()) + 1;
+    map.clear();
+    if (max > res) res = max;
+  }
+  return res;
 };
+
+console.log(maxPoints([[0,0],[1,-1],[1,1]]));
--- 真题/链表/（未）反转链表2.js
+++ 真题/链表/（未）反转链表2.js
@@ -7,20 +7,53 @@
  * 例1：
  * 输入: 1->2->3->4->5->NULL, m = 2, n = 4
  * 输出: 1->4->3->2->5->NULL
- * 
- * 
+ *  
  * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
+ *   this.val = (val===undefined ? 0 : val)
+ *   this.next = (next===undefined ? null : next)
  * }
+ * 
+ * 解题思路：
+ * 
+ * 只需要反转m到n的链表，只需要遍历这一段，将每一个值都依次放到这段最左边即可。
+ * 1->2->3->4->5->NULL, m = 2, n = 4
+ * 依次遍历
+ * 一. 3放到这一段最左侧
+ * 1->3->2->4->5->NULL
+ * 二. 4放到这一段最左侧
+ * 1->4->3->2->5->NULL
+ * 三. 完成
+ * 
+ * 我们只需要通过三个值就可以完成位置互换
+ * m的前一个值pre, 当前值cur（一直都是m初始位置的那个值，只是位置在不停交换）,cur的下一个值next
  */
-// 实现一个链表的创建以及打印的方法
-var reverseBetween = function(head, left, right) {
+import { createLinkedList, printfLinkedList } from '@/utils';
 
+var reverseBetween = function(head, left, right) {
+  // 链表问题的一般处理，不需要再去处理head的变更
+  let dummyNode = new ListNode(-1);
+  dummyNode.next = head;
+  let pre = dummyNode;
+  for (let i = 0; i < left - 1; i ++) {
+    // 遍历得到left前一个位置的链表
+    pre = pre.next;
+  }
+  // 得到left位置的链表，作为当前cur
+  let cur = pre.next, next;
+  // 遍历需要处理的位置间隔
+  for (let i = 0; i < right - left; i ++) {
+    next = cur.next;
+    cur.next = next.next;
+    next.next = pre.next;
+    pre.next = next;
+  }
+  return dummyNode.next;
 };
 
-var test = [1,2,3,4,5];
 
 
+var linkList = createLinkedList([1,2,3,4,5]);
+console.log(printfLinkedList(linkList));
 
-console.log(printfLinkedList(createLinkedList(test)));
+var res = reverseBetween(linkList, 2, 4);
+console.log(printfLinkedList(res));
