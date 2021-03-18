00cf209e39d242b15235bd811dd43365151a78e3
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-03-18 15:34:01 +0800

    feat:链表-合并两个有序链表

--- 真题/链表/合并两个有序链表.js
+++ 真题/链表/合并两个有序链表.js
@@ -18,8 +18,26 @@
  * 两个链表的节点数目范围是 [0, 50]
  * -100 <= Node.val <= 100
  * l1 和 l2 均按 非递减顺序 排列
+ * 
+ * 解题思路：
+ * 归并排序的思路，两个链表，从头开始两两比较
  */
 
 var mergeTwoLists = function(l1, l2) {
-
+  if (!l1 || !l2) return l1 || l2;
+  const dummy = new ListNode(-1);
+  let left = l1, right = l2, res = dummy;
+  while(left || right) {
+    let cur;
+    if (right && (!left || (left.val > right.val))) {
+      cur = right;
+      right = right.next;
+    } else {
+      cur = left;
+      left = left.next;
+    }
+    res.next = cur;
+    res = cur;
+  }
+  return dummy.next
 };
