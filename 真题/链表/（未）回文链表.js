bb2f49dff42b37eab0c0c7c9af30e0385b5dd4cd
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-03-18 22:29:45 +0800

    'feat:链表完成'

---
+++ 真题/链表/（未）回文链表.js
@@ -0,0 +1,25 @@
+/***
+ * leetcode 234
+ * 请判断一个链表是否为回文链表。
+ * 
+ * 用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题
+ * 
+ * 例1：
+ * 输入: 1->2
+ * 输出: false
+ * 
+ * 例2：
+ * 输入: 1->2->2->1
+ * 输出: true
+ * 
+ * 
+ * 解题思路：
+ * 
+ *
+ */
+
+import { createLinkedList, printfLinkedList, ListNode } from '@/utils';
+
+var isPalindrome = function(head) {
+
+};
---
+++ 真题/链表/（未）旋转链表.js
@@ -0,0 +1,27 @@
+/***
+ * leetcode 61
+ * 给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+ * 
+ * 
+ * 例1：
+ * 输入: 1->2->3->4->5->NULL, k = 2
+ * 输出: 4->5->1->2->3->NULL
+ * 解释:
+ * 向右旋转 1 步: 5->1->2->3->4->NULL
+ * 向右旋转 2 步: 4->5->1->2->3->NULL
+ * 
+ * 例2：
+ * 输入: 0->1->2->NULL, k = 4
+ * 输出: 2->0->1->NULL
+ * 
+ * 
+ * 解题思路：
+ * 
+ *
+ */
+
+import { createLinkedList, printfLinkedList, ListNode } from '@/utils';
+
+var rotateRight = function(head, k) {
+
+};
