075437a7dd6b2ac3ed472d8cbe4ea297981975c7
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-03-19 21:42:39 +0800

    *

--- 真题/链表/（未）回文链表.js
+++ 真题/链表/（未）回文链表.js
@@ -23,3 +23,42 @@ import { createLinkedList, printfLinkedList, ListNode } from '@/utils';
 var isPalindrome = function(head) {
 
 };
+
+var test1 = createLinkedList([1,2]);
+var test2 = createLinkedList([1,2,2,1]);
+
+console.log(isPalindrome(test1));
+console.log(isPalindrome(test2));
+
+
+const isPalindrome = (head) => {
+  if (head == null || head.next == null) {
+    return true;
+  }
+  let fast = head;
+  let slow = head;
+  let prev;
+  while (fast && fast.next) {
+    prev = slow;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  prev.next = null;  // 断成两个链表
+  // 翻转后半段
+  let head2 = null;
+  while (slow) {
+    const tmp = slow.next;
+    slow.next = head2;
+    head2 = slow;
+    slow = tmp;
+  }
+  // 比对
+  while (head && head2) {
+    if (head.val != head2.val) {
+      return false;
+    }
+    head = head.next;
+    head2 = head2.next;
+  }
+  return true;
+};
