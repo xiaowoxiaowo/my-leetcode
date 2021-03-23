0ff063f92b882c5c23f7242ca1b58321736b5101
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-03-23 20:01:36 +0800

    *

--- 真题/二叉树/（未）扁平化嵌套列表迭代器.js
+++ 真题/二叉树/（未）扁平化嵌套列表迭代器.js
@@ -12,6 +12,11 @@
  * 输出: [1,4,6]
  * 解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,4,6]。
  * 
+ * 自带有三个方法，isInteger判断是否是整数，getInteger获取整数，getList获取列表
+ * 
+ * 需要实现这样的功能
+ * var i = new NestedIterator(nestedList), a = [];
+ * while (i.hasNext()) a.push(i.next());
  * 
  * 解题思路：
  * 
@@ -19,14 +24,61 @@
  * 
  */
 
+// 深度优先搜索
+var NestedIterator = function (nestedList) {
+  let res = [];
+  const dfs = (node) => {
+    for(let i = 0; i < node.length; i ++) {
+      let cur = node[i];
+      if (cur.isInteger()) {
+        res.push(cur.getInteger());
+      } else {
+        dfs(cur.getList());
+      }
+    }
+  };
+  dfs(nestedList);
+};
+
+NestedIterator.prototype.hasNext = function () {
+
+};
+
+NestedIterator.prototype.next = function () {
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 栈思路
 var NestedIterator = function(nestedList) {
-    
+  this.stack = nestedList;
 };
 
 NestedIterator.prototype.hasNext = function() {
-    
+  while (this.stack.length !== 0) {
+      if (this.stack[0].isInteger()) {
+          return true;
+      } else {
+          let cur = this.stack[0].getList();
+          this.stack.shift();
+          this.stack.unshift(...cur);
+      }
+  }
 };
 
 NestedIterator.prototype.next = function() {
-    
+  return this.stack.shift().getInteger();
 };
