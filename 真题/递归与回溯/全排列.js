6326f8409d332b3af28aa97af256793b450ee9a9
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-03-31 00:15:28 +0800

    feat:递归和回溯

---
+++ 真题/递归与回溯/全排列.js
@@ -0,0 +1,40 @@
+/***
+ * leetcode 46
+ * 给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+ * 
+ * 
+ * 例1：
+ * 输入：[1,2,3]
+ * 输出：
+ * [
+ * [1,2,3],
+ * [1,3,2],
+ * [2,1,3],
+ * [2,3,1],
+ * [3,1,2],
+ * [3,2,1]
+ * ]
+ * 
+ * 解题思路：
+ * 递归遍历
+ */
+
+var permute = function(nums) {
+	if (nums.length <= 1) return [nums];
+	let res = [];
+	// 递归方法
+	const loop = (arr, s) => {
+		// 如果arr长度为1，返回结果
+		if (arr.length === 1) return res.push([...s, arr[0]]);
+		// 遍历数组元素
+		for(let i = 0; i < arr.length; i ++) {
+			let cur = [...arr];
+			let val = arr[i];
+			cur.splice(i, 1);
+			// 取出当前取值的元素以及路径数组传入下一层循环
+			loop(cur, [...s, val])
+		}
+	};
+	loop(nums, []);
+	return res;
+};
--- 真题/递归与回溯/电话号码的字母组合.js
+++ 真题/递归与回溯/电话号码的字母组合.js
@@ -30,8 +30,38 @@
  * digits[i] 是范围 ['2', '9'] 的一个数字。
  * 
  * 解题思路：
+ * 递归，对于每个数字对应的字符串中每个字母，都进行递归
+ * 时间复杂度是 3^n，常数的n次方的时间复杂度，都可以表示成O(2^n)
  */
 
-var letterCombinations = function(digits) {
+const LETTER_MAP = {
+	'2': 'abc',
+	'3': 'def',
+	'4': 'ghi',
+	'5': 'jkl',
+	'6': 'mno',
+	'7': 'pqrs',
+	'8': 'tuv',
+	'9': 'wxyz',
+	res: []
+};
 
+
+var findCombination = function(digits, index, str) {
+	// 递归终止条件，当index === digits的长度时，当前链路结束，将参数推入结果数组中
+	if (index >= digits.length) return LETTER_MAP.res.push(str);
+	// 获取当前数字对应的字符串
+	let letters = LETTER_MAP[digits[index]];
+	// 将字符串中每个包含的字母，都进行递归
+	for (let i = 0; i < letters.length; i ++) {
+		findCombination(digits, index + 1, str + letters[i]);
+	}
+}
+
+var letterCombinations = function(digits) {
+	if (!digits) return [];
+	LETTER_MAP.res = [];
+	// 递归
+	findCombination(digits, 0, '');
+	return LETTER_MAP.res;
 };
