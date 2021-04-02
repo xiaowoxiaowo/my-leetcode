e5217f24f1272e43668ecebf2fae510c666150b9
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-04-02 17:54:29 +0800

    feat:组合总和2

--- 真题/递归与回溯/（未）组合总和2.js
+++ 真题/递归与回溯/（未）组合总和2.js
@@ -25,9 +25,35 @@
  * ]
  * 
  * 解题思路：
- * 
+ * 组合总和 的升级版题目，思路一致，本题重点在于处理candidates中的重复数据，递归的同一层级里，不能重复递归相同的元素，
+ * 不然会产生重复的数据，因为我们一开始对数据进行了排序，所以在同一层级中，只需要判断当前元素是否跟上一个元素相同即可，
+ * 只有当不相同时，才去继续递归。只需要加一句代码即可
  */
 
 var combinationSum2 = function(candidates, target) {
-
+  if (candidates.length < 1) return [];
+  let res = [];
+  // 先进行排序
+  candidates.sort((a,b) => a - b);
+  const loop = (val, arr, start) => {
+    // 这里控制循环的起始节点
+    for (let i = start; i < candidates.length; i ++) {
+      // 跟 组合总和 相比，其实就是多了这句代码
+      // 当前的元素必须跟上一个元素不一致
+      // 否则跳出当前循环，进入下一次循环
+      if ((i - 1) >= start  && candidates[i] === candidates[i - 1]) continue;
+      let curVal = val + Number(candidates[i]);
+      if (curVal < target) {
+        loop(curVal, [...arr, candidates[i]], i + 1);
+      } else if (curVal === target) {
+        return res.push([...arr, candidates[i]]);
+      } else {
+        return;
+      }
+    }
+  };
+  loop(0, [], 0);
+  return res;
 };
+
+console.log(combinationSum2([10,1,2,7,6,1,5], 8));
