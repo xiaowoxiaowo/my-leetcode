878a294822c360dfc66b235c95c9257a3d436cc7
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-04-15 20:05:02 +0800

    feat:不同路径

--- 真题/动态规划/（未）不同路径.js
+++ 真题/动态规划/（未）不同路径.js
@@ -21,5 +21,59 @@
  * 解题思路：
  */
 var uniquePaths = function(m, n) {
-
+  let path = [];
+  for (let i = 0; i < m; i ++) {
+    if (i === 0) {
+      path.push(new Array(n).fill(1));
+    } else {
+      path.push(new Array(n).fill(0));
+      path[i][0] = 1;
+    }
+  }
+  for (let i = 2; i < m; i ++) {
+    for(let j = 2; j < n; j ++) {
+      path[i][j] = path[i - 1][j] + path[i][j - 1];
+    }
+  }
+  return path[m - 1][n - 1];
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var uniquePaths = function(m, n) {
+//   const f = new Array(m).fill(0).map(() => new Array(n).fill(0));
+//   for (let i = 0; i < m; i++) {
+//       f[i][0] = 1;
+//   }
+//   for (let j = 0; j < n; j++) {
+//       f[0][j] = 1;
+//   }
+//   for (let i = 1; i < m; i++) {
+//       for (let j = 1; j < n; j++) {
+//           f[i][j] = f[i - 1][j] + f[i][j - 1];
+//       }
+//   }
+//   return f[m - 1][n - 1];
+// };
+
+
+// var uniquePaths = function(m, n) {
+//   let ans = 1;
+//   for (let x = n, y = 1; y < m; ++x, ++y) {
+//       ans = Math.floor(ans * x / y);
+//   }
+//   return ans;
+// };
