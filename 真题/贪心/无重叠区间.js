bc9c893aa2804a4706b5f05216b48bd08e08cfc9
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-04-23 00:05:54 +0800

    feat:无重叠区间

--- 真题/贪心/无重叠区间.js
+++ 真题/贪心/无重叠区间.js
@@ -24,17 +24,9 @@
 var eraseOverlapIntervals = function(intervals) {
 	const len = intervals.length;
 	if (len === 0 || len === 1) return 0;
-	// 排序，起点从小到大排列，如果两个节点起点大小一致，按终点大小从小到大排列
-	intervals = intervals.sort((a, b) => {
-		if (a[0] !== b[0]) {
-			return a[0] - b[0];
-		} else {
-			return a[a.length - 1] - b[b.length - 1];
-		}
-	});
+	intervals = intervals.sort((a, b) => a[0] - b[0]);
 	// 这个dp代表每个以i节点值作为终点的最长无重叠区间长度
-	let dp = new Array(len).fill(-1), res = 1;
-	dp[0] = 1;
+	let dp = new Array(len).fill(1), res = 1;
 	for (let i = 1; i < len; i ++) {
 		for (let j = i; j >= 0; j --) {
 			const cl = intervals[j].length;
@@ -42,25 +34,27 @@ var eraseOverlapIntervals = function(intervals) {
 				dp[i] = Math.max(dp[i], 1 + dp[j]);
 			}
 		}
-		if (dp[i] === -1) {
-			dp[i] = 1;
-		} else {
-			res = Math.max(res, dp[i]);
-		}
+		res = Math.max(res, dp[i]);
 	}
 	return len - res;
 };
 
 // 使用贪心算法进行优化
+// 每个区间的结尾越小，留给后面的空间也就越大
+// 按照区间的结尾排序，每次选择结尾最早的，且和前一个区间不重叠的区间
 var eraseOverlapIntervals = function(intervals) {
 	const len = intervals.length;
 	if (len === 0 || len === 1) return 0;
-	intervals = intervals.sort((a, b) => {
-		if (a[0] !== b[0]) {
-			return a[0] - b[0];
-		} else {
-			return a[a.length - 1] - b[b.length - 1];
+	// 排序，终点从小到大排列
+	intervals = intervals.sort((a, b) => a[1] - b[1]);
+	let res = 1, pre = 0;
+	// 因为数组现在是按终点大小从小到大排序的，所以如果后一个节点满足起点大于等于前一个节点的终点
+	// 该解其实就是最优解，因为他们的结尾都是当前数组中最小的
+	for (let i = 1; i < len; i ++) {
+		if (intervals[i][0] >= intervals[pre][1]) {
+			res ++;
+			pre = i;
 		}
-	});
-
+	}
+	return len - res;
 }
