8e7d3bdad2616c685718293eadc81eaa7f3ba285
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-05-06 19:59:54 +0800

    feat:跳跃游戏

--- 真题/贪心/判断子序列.js
+++ 真题/贪心/判断子序列.js
@@ -23,5 +23,14 @@
  * 
  */
 var isSubsequence = function(s, t) {
-
+  if (!s && !t) return false;
+  let i = 0;
+  for (let j = 0; j < t.length; j ++) {
+    if (t[j] === s[i]) i++;
+    if (i >= s.length) return true;
+  }
+  return false;
 };
+
+console.log(isSubsequence("abc", "ahbgdc"));
+console.log(isSubsequence("axc", "ahbgdc"));
