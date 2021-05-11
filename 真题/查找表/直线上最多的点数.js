4232c46e2bd00ce4c195c8ec9b1beb9013193129
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-05-11 20:32:00 +0800

    *

--- 真题/查找表/直线上最多的点数.js
+++ 真题/查找表/直线上最多的点数.js
@@ -58,32 +58,6 @@ var maxPoints = function(points) {
   return res;
 };
 
-var maxPoints1 = function(points) {
-	points.sort((a,b)=>a[0]-b[0]);
-	console.log(points);
-	let max = 1;
-	const dfs = (startIndex, selected)=>{
-			max = Math.max(max, selected.length); // 更新最大值
-			for (let i = startIndex; i<points.length; ++i){
-					if (selected.length < 2){ // 若当前数组元素小于2个，则无法计算斜率，继续无脑添加：）
-							selected.push(points[i]);
-							dfs(i+1, selected);
-							selected.pop(); // 注意需要回溯一下，考虑不添加这个点的情况
-					}else{
-							const curK = (selected[0][0] - selected[1][0]) / (selected[0][1] - selected[1][1]); /* 当前数组中直线的斜率 */
-							const thisK = (selected[0][0] - points[i][0]) / (selected[0][1] - points[i][1]); /* 新的点与数组中任一点的斜率 */
-							if (curK === thisK){ /* 若新的点在直线上 */
-									selected.push(points[i]);
-									dfs(i+1, selected);
-									selected.pop(); /* 回溯一下 */
-							}
-					}
-			}
-	}
-	dfs(0, []);
-	return max;
-};
-
 
 // console.log(maxPoints([[-9,-651],[-4,-4],[-8,-582],[9,591],[-3,3]]));
 console.log(maxPoints1([[1,1], [2, 2]]));
