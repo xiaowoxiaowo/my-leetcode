a04463e07ef1ae6e775f8aaa578328cb339d0b0d
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-05-14 15:28:20 +0800

    *

--- 真题/二叉树/（递归）二叉树的所有路径.js
+++ 真题/二叉树/（递归）二叉树的所有路径.js
@@ -18,23 +18,39 @@
  * 递归的思路，从底部的叶子节点开始向上传递一个数组，每一层都给返回的数组中的字符串中添加自己节点的值。
  */
 
+// var binaryTreePaths = function(root) {
+// 	if (!root) return [];
+// 	let res = [];
+// 	// 如果当前节点是叶子节点,将当前节点值push进res，返回上层
+// 	if (!root.left && !root.right) {
+// 		res.push(String(root.val));
+// 		return res;
+// 	}
+// 	// 左侧树，给下层返回的res数组中的每个元素添加当前节点的val值
+// 	let leftList = binaryTreePaths(root.left);
+// 	for (let i = 0; i < leftList.length; i ++) {
+// 		res.push(`${root.val}->${leftList[i]}`);
+// 	}
+// 	// 右侧树，给下层返回的res数组中的每个元素添加当前节点的val值
+// 	let rightList = binaryTreePaths(root.right);
+// 	for (let i = 0; i < rightList.length; i ++) {
+// 		res.push(`${root.val}->${rightList[i]}`);
+// 	}
+// 	return res;
+// };
+
+
+// 更加习惯的递归方法
 var binaryTreePaths = function(root) {
 	if (!root) return [];
-	let res = [];
-	// 如果当前节点是叶子节点,将当前节点值push进res，返回上层
-	if (!root.left && !root.right) {
-		res.push(String(root.val));
-		return res;
-	}
-	// 左侧树，给下层返回的res数组中的每个元素添加当前节点的val值
-	let leftList = binaryTreePaths(root.left);
-	for (let i = 0; i < leftList.length; i ++) {
-		res.push(`${root.val}->${leftList[i]}`);
-	}
-	// 右侧树，给下层返回的res数组中的每个元素添加当前节点的val值
-	let rightList = binaryTreePaths(root.right);
-	for (let i = 0; i < rightList.length; i ++) {
-		res.push(`${root.val}->${rightList[i]}`);
-	}
+	if (!root.left && !root.right) return [String(root.val)];
+	const res = [];
+	const loop = (node, arr) => {
+		const newArr = [...arr, String(node.val)];
+		if (!node.left && !node.right) return res.push(newArr.join('->'));
+		if (node.left) loop(node.left, newArr);
+		if (node.right) loop(node.right, newArr);
+	};
+	loop(root, []);
 	return res;
-};
+}
