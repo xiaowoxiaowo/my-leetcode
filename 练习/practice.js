7e68a0e4c12e6a6288f863b37baf50738ef0644d
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-06-01 20:27:40 +0800

    featLRU

--- 练习/practice.js
+++ 练习/practice.js
@@ -1,49 +1,68 @@
 /***
- * leetcode 1744 你能在你最喜欢的那天吃到你最喜欢的糖果吗？
+ * leetcode 146. LRU 缓存机制
  * 
- * 给你一个下标从 0 开始的正整数数组 candiesCount ，其中 candiesCount[i] 表示你拥有的第 i 类糖果的数目。
- * 同时给你一个二维数组 queries ，其中 queries[i] = [favoriteTypei, favoriteDayi, dailyCapi] 。
+ * 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制 。实现 LRUCache 类：
  * 
- * 你按照如下规则进行一场游戏：
+ * - LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存
+ * - int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+ * - void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。
+ *   当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
  * 
- * - 你从第 0 天开始吃糖果。
- * - 你在吃完 所有 第 i - 1 类糖果之前，不能 吃任何一颗第 i 类糖果。
- * - 在吃完所有糖果之前，你必须每天 至少 吃 一颗 糖果。
+ * 输入 ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+ *      [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+ * 输出  [null, null, null, 1, null, -1, null, -1, 3, 4]
+ * 解释
+ * LRUCache lRUCache = new LRUCache(2);
+ * lRUCache.put(1, 1); // 缓存是 {1=1}
+ * lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+ * lRUCache.get(1);    // 返回 1
+ * lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+ * lRUCache.get(2);    // 返回 -1 (未找到)
+ * lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+ * lRUCache.get(1);    // 返回 -1 (未找到)
+ * lRUCache.get(3);    // 返回 3
+ * lRUCache.get(4);    // 返回 4
  * 
- * 请你构建一个布尔型数组 answer ，满足 answer.length == queries.length 。
- * answer[i] 为 true 的条件是：在每天吃 不超过 dailyCapi 颗糖果的前提下，你可以在第 favoriteDayi 天吃到第 favoriteTypei 类糖果；
- * 否则 answer[i] 为 false 。注意，只要满足上面 3 条规则中的第二条规则，你就可以在同一天吃不同类型的糖果。
+ * 进阶：你是否可以在 O(1) 时间复杂度内完成这两种操作？
  * 
- * 请你返回得到的数组 answer 。
+ * ["LRUCache","put","put","get","put","put","get"]
+ * [[2],[2,1],[2,2],[2],[1,1],[4,1],[2]]
  * 
- * 
- * 输入：candiesCount = [7,4,5,3,8], queries = [[0,2,2],[4,2,4],[2,13,1000000000]]
- * 输出：[true,false,true]
- * 提示：
- * 1- 在第 0 天吃 2 颗糖果(类型 0），第 1 天吃 2 颗糖果（类型 0），第 2 天你可以吃到类型 0 的糖果。
- * 2- 每天你最多吃 4 颗糖果。即使第 0 天吃 4 颗糖果（类型 0），第 1 天吃 4 颗糖果（类型 0 和类型 1），你也没办法在第 2 天吃到类型 4 的糖果。换言之，你没法在每天吃 4 颗糖果的限制下在第 2 天吃到第 4 类糖果。
- * 3- 如果你每天吃 1 颗糖果，你可以在第 13 天吃到类型 2 的糖果。
- * 
- * 输入：candiesCount = [5,2,6,4,1], queries = [[3,1,2],[4,10,3],[3,10,100],[4,100,30],[1,3,1]]
- * 输出：[false,true,true,false,false]
- * 
- * 说明：
- * queries[i].length == 3
- * 0 <= favoriteTypei < candiesCount.length
+ * [null,null,null,2,null,null,undefined]
  */
 
-var canEat = function(candiesCount, queries) {
-  const res = new Array(queries.length).fill(false);
-  const dp = new Array(candiesCount.length).fill(candiesCount[0]);
-  for (let i = 1; i < dp.length; i ++) {
-    dp[i] = dp[i - 1] + candiesCount[i];
-  }
-  dp[-1] = -Infinity;
-  for (let i = 0; i < queries.length; i ++) {
-    const [type, day, max] = queries[i];
-    res[i] = dp[type] > day && dp[type - 1] < (max * (day + 1));
-  }
-  return res;
+var LRUCache = function(capacity) {
+  this.maxCache = capacity;
+  this.cache = new Map();
+  this.LRUArr = [];
 };
 
-console.log(canEat([5,2,6,4,1], [[3,1,2],[4,10,3],[3,10,100],[4,100,30],[1,3,1]]));
+LRUCache.prototype.get = function(key) {
+  // console.log(key, this.LRUArr, this.cache);
+  const index = this.LRUArr.indexOf(key);
+  if (index === -1) return -1;
+  [this.LRUArr[index], this.LRUArr[0]] = [this.LRUArr[0], this.LRUArr[index]];
+  return this.cache.get(key);
+};
+
+LRUCache.prototype.put = function(key, value) {
+  const size = this.cache.size;
+  console.log(size,  this.maxCache);
+  if (size >= this.maxCache) {
+    const deleteKey = this.LRUArr.pop();
+    console.log('============');
+    console.log(this.LRUArr.length);
+    this.cache.delete(deleteKey);
+  }
+  this.cache.set(key, value);
+  this.LRUArr.unshift(key);
+  console.log(this.LRUArr.length);
+};
+
+var a = new LRUCache(2);
+a.put(2,1);
+a.put(2,2);
+a.get(2);
+a.put(1,1);
+a.put(4,1);
+a.get(2);
