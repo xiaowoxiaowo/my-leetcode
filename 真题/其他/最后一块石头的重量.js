65a5ea4621622291769851f17bea7e05f561f164
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-06-08 18:04:16 +0800

    feat:最后一块石头重量

---
+++ 真题/其他/最后一块石头的重量.js
@@ -0,0 +1,54 @@
+/***
+ * leetcode 1046 最后一块石头的重量
+ * 
+ * 有一堆石头，用整数数组 stones 表示。其中 stones[i] 表示第 i 块石头的重量。
+ * 每一回合，从中选出两块 最重的 石头，然后将它们一起粉碎。假设石头的重量分别为 x 和 y，且 x <= y。那么粉碎的可能结果如下：
+ *  - 如果 x == y，那么两块石头都会被完全粉碎；
+ *  - 如果 x != y，那么重量为 x 的石头将会完全粉碎，而重量为 y 的石头新重量为 y-x。
+ * 
+ * 最后，最多只会剩下一块 石头。返回此石头 最小的可能重量 。如果没有石头剩下，就返回 0。
+ * 
+ * 
+ * 输入：stones = [2,7,4,1,8,1]
+ * 输出：1
+ * 解释：
+ * 组合 2 和 4，得到 2，所以数组转化为 [2,7,1,8,1]，
+ * 组合 7 和 8，得到 1，所以数组转化为 [2,1,1,1]，
+ * 组合 2 和 1，得到 1，所以数组转化为 [1,1,1]，
+ * 组合 1 和 1，得到 0，所以数组转化为 [1]，这就是最优值。
+ * 
+ */
+// 每次挑出两块最重的石头，每次对数组进行一次排序
+var lastStoneWeight = function(stones) {
+  const len = stones.length;
+  if (len === 0) return 0;
+  if (len === 1) return stones[0];
+  let arr = stones.sort((a, b) => b - a);
+  while (arr.length > 1) {
+    let res = [];
+    for (let i = 0; i < arr.length; i += 2) {
+      if (i === arr.length - 1) return arr[i];
+      if (arr[i] !== arr[i + 1]) {
+        res = [arr[i] - arr[i + 1], ...arr.slice(i + 2)]
+        break;
+      }
+    }
+    arr = res.sort((a, b) => b - a);
+  }
+  if (arr.length === 0) return 0
+  return arr[0];
+};
+
+// 用递归的思路减少代码量
+var lastStoneWeight = function(stones) {
+  stones.sort((a, b) => a - b);
+  if (stones.length > 1) {
+      const d = stones.pop() - stones.pop();
+      if (d) stones.push(d);
+      return lastStoneWeight(stones);
+  }
+  return stones.length ? stones[0] : 0;
+};
+
+
+console.log(lastStoneWeightII([31,26,33,21,40]));
