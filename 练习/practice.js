8bda80bfc5f40344e36a3b460ebb2b2eced6b282
xiaowoxiaowo <awlzluggeoo@sina.cn>
2021-06-20 16:50:17 +0800

    *

--- 练习/practice.js
+++ 练习/practice.js
@@ -1,25 +1,35 @@
 /**
- * leetcode 483. 最小好进制
+ * leetcode 1600. 皇位继承顺序
  *
- * 对于给定的整数 n, 如果n的k（k>=2）进制数的所有数位全为1，则称 k（k>=2）是 n 的一个好进制。
- * 以字符串的形式给出 n, 以字符串的形式返回 n 的最小好进制。
+ * 一个王国里住着国王、他的孩子们、他的孙子们等等。每一个时间点，这个家庭里有人出生也有人死亡。
  * 
- * 输入："13"
- * 输出："3"
- * 解释：13 的 3 进制是 111。
+ * 这个王国有一个明确规定的皇位继承顺序，第一继承人总是国王自己。我们定义递归函数 Successor(x, curOrder) ，
+ * 给定一个人 x 和当前的继承顺序，该函数返回 x 的下一继承人。
  * 
- * 输入："4681"
- * 输出："8"
- * 解释：4681 的 8 进制是 11111。
+ * Successor(x, curOrder):
+ * 如果 x 没有孩子或者所有 x 的孩子都在 curOrder 中：
+ *  如果 x 是国王，那么返回 null
+ *  否则，返回 Successor(x 的父亲, curOrder)
+ *  否则，返回 x 不在 curOrder 中最年长的孩子
  * 
- * 输入："1000000000000000000"
- * 输出："999999999999999999"
- * 解释：1000000000000000000 的 999999999999999999 进制是 11。
+ * 比方说，假设王国由国王，他的孩子 Alice 和 Bob （Alice 比 Bob 年长）和 Alice 的孩子 Jack 组成。
  * 
- * 说明：
- * 输入总是有效且没有前导 0。
+ * 一开始， curOrder 为 ["king"].
+ * 调用 Successor(king, curOrder) ，返回 Alice ，所以我们将 Alice 放入 curOrder 中，得到 ["king", "Alice"] 。
+ * 调用 Successor(Alice, curOrder) ，返回 Jack ，所以我们将 Jack 放入 curOrder 中，得到 ["king", "Alice", "Jack"] 。
+ * 调用 Successor(Jack, curOrder) ，返回 Bob ，所以我们将 Bob 放入 curOrder 中，得到 ["king", "Alice", "Jack", "Bob"] 。
+ * 调用 Successor(Bob, curOrder) ，返回 null 。最终得到继承顺序为 ["king", "Alice", "Jack", "Bob"] 。
+ * 
+ * 通过以上的函数，我们总是能得到一个唯一的继承顺序。
+ * 请你实现 ThroneInheritance 类：
+ * ThroneInheritance(string kingName) 初始化一个 ThroneInheritance 类的对象。国王的名字作为构造函数的参数传入。
+void birth(string parentName, string childName) 表示 parentName 新拥有了一个名为 childName 的孩子。
+void death(string name) 表示名为 name 的人死亡。一个人的死亡不会影响 Successor 函数，也不会影响当前的继承顺序。你可以只将这个人标记为死亡状态。
+string[] getInheritanceOrder() 返回 除去 死亡人员的当前继承顺序列表。
+
+
  */
-// 暴力解法，时间会超出，可以通过二分法进行优化
+
 var smallestGoodBase = function(n) {
 	n = Number(n);
 	const len = Math.ceil(Math.log2(n));
